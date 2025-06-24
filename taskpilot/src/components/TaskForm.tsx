1e3f4a713b4991f7bd000a34baab209efc4d357b
Pedro Szpak <dropszpalves@gmail.com>
2025-06-24 13:42:53 -0300

    feat: create form component to handle user input

---
+++ taskpilot/src/components/TaskForm.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+type Props = {
+  btnText: string
+}
+
+const TaskForm = ({btnText}: Props) => {
+  return <form>
+    <div>
+      <label htmlFor="title">Título:</label>
+      <input type="text" name='title' placeholder='Título da Tarefa' />
+    </div>
+    <div>
+      <label htmlFor="difficulty">Dificuldade</label>
+      <input type="text" name='difficulty' placeholder='Dificuldade da Tarefa' />
+    </div>
+    <input type="submit" value={btnText} />
+  </form>
+}
+
+export default TaskForm
