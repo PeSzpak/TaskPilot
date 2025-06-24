9eed2d7607a10725857e3fca666702af87322f5d
Pedro Szpak <dropszpalves@gmail.com>
2025-06-24 14:27:56 -0300

    feat: implement functionality to add new tasks from form input

--- taskpilot/src/App.tsx
+++ taskpilot/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 //components
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -8,13 +8,17 @@ import TaskForm from './components/TaskForm';
 import TaskList from './components/TaskList';
 import { ITask } from './interfaces/Task';
 function App() {
+
+  const [taskList, setTaskList] = useState<ITask[]>([])
+
+
   return (
     <div>
       <Header />
       <main className={styles.main}>
         <div>
           <h2>O que você vai fazer?</h2>
-          <TaskForm btnText='Criar Tarefa' />
+          <TaskForm btnText='Criar Tarefa' taskList={taskList} setTaskList={setTaskList} />
         </div>
         <div>
           <h2>Suas Tarefas:</h2>
--- taskpilot/src/components/TaskForm.tsx
+++ taskpilot/src/components/TaskForm.tsx
@@ -3,15 +3,28 @@ import styles from './TaskForm.module.css'
 import { ITask } from '../interfaces/Task'
 type Props = {
   btnText: string
+  taskList: ITask[]
+  setTaskList?: React.Dispatch<React.SetStateAction<ITask[]>>
 }
 
-const TaskForm = ({ btnText }: Props) => {
+const TaskForm = ({ btnText, taskList, setTaskList}: Props) => {
 
   const [id, setId] = useState<number>(0)
   const [title, setTitle] = useState<string>("")
   const [difficulty, setDifficulty] = useState<number>(0)
 
-  const addTaskHandle = () => {}
+  const addTaskHandle = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const id = Math.floor(Math.random() * 1000)
+
+    const newTask: ITask = {id, title, difficulty}
+
+    setTaskList!([...taskList, newTask])
+
+    setTitle("")
+    setDifficulty(0)
+  }
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     if(e.target.name === 'title') {
       setTitle(e.target.value)
@@ -25,11 +38,11 @@ const TaskForm = ({ btnText }: Props) => {
   return (<form onSubmit={addTaskHandle} className={styles.form}>
     <div className={styles.input_container}>
       <label htmlFor="title">Título:</label>
-      <input type="text" name='title' placeholder='Título da Tarefa' onChange={handleChange} />
+      <input type="text" name='title' placeholder='Título da Tarefa' onChange={handleChange} value={title}/>
     </div>
     <div className={styles.input_container}>
       <label htmlFor="difficulty">Dificuldade</label>
-      <input type="text" name='difficulty' placeholder='Dificuldade da Tarefa' onChange={handleChange} />
+      <input type="text" name='difficulty' placeholder='Dificuldade da Tarefa' onChange={handleChange} value={difficulty}/>
     </div>
     <input type="submit" value={btnText} />
   </form>
