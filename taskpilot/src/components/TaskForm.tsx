81f9fafccbab9c8da3f41e73cbe5592796e38e79
Pedro Szpak <dropszpalves@gmail.com>
2025-06-24 13:58:13 -0300

    style: apply scoped CSS Modules to form component

--- taskpilot/src/components/TaskForm.tsx
+++ taskpilot/src/components/TaskForm.tsx
@@ -1,21 +1,22 @@
 import React from 'react'
-
+import styles from './TaskForm.module.css'
 type Props = {
   btnText: string
 }
 
 const TaskForm = ({btnText}: Props) => {
-  return <form>
-    <div>
+  return (<form className={styles.form}>
+    <div className={styles.input_container}>
       <label htmlFor="title">Título:</label>
       <input type="text" name='title' placeholder='Título da Tarefa' />
     </div>
-    <div>
+    <div className={styles.input_container}>
       <label htmlFor="difficulty">Dificuldade</label>
       <input type="text" name='difficulty' placeholder='Dificuldade da Tarefa' />
     </div>
     <input type="submit" value={btnText} />
   </form>
+  )
 }
 
 export default TaskForm
